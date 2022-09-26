6a4463bae2c1a2c2d7ac1d3630b15feda6ec5a72
shan869 <freezhan@gmail.com>
2022-09-26 19:56:57 +0800

    Update hongen.js

--- QuantumultX/hongen.js
+++ QuantumultX/hongen.js
@@ -22,7 +22,7 @@ MITM = bookapi.ihuman.com
 const path1 = "/v1/get_purchase_list";
 const path2 = "/v1/get_user_info";
 
-let key = {"expire_time":7955110875,"vip_type":YEAD,"last_product_id":"com.ihuman.book.sub.vip1y"};
+let key = {"expire_time":7955110875,"vip_type":1,"last_product_id":"com.ihuman.book.sub.vip1y"};
 
 let obj = JSON.parse($response.body);
 
