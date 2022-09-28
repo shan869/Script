257db781f8a868cae4cb0f6372db1b8898fac258
shan869 <freezhan@gmail.com>
2022-09-28 16:56:59 +0800

    Update hn.js

--- QuantumultX/hn.js
+++ QuantumultX/hn.js
@@ -16,7 +16,7 @@ QX:
 
 [rewrite_local]👇
 
-https:\/\/bookapi\.ihuman\.com\/(v1\/get\_user\_info|v1\/get\_purchase\_list|v1\/get\_vip\_info) url script-response-body hnsyhb.js
+https:\/\/bookapi\.ihuman\.com\/(v1\/get\_user\_info|v1\/get\_purchase\_list|v1\/get\_vip\_info|v3\/get\_home\_info) url script-response-body hnsyhb.js
 
 
 MITM = bookapi.ihuman.com
@@ -26,9 +26,12 @@ MITM = bookapi.ihuman.com
 const path1 = "/v1/get_purchase_list";
 const path2 = "/v1/get_user_info";
 const path3 = "/v1/get_vip_info";
-
+const path4 = "/v3/get_home_info";
 
 let key = {"vip_type":2,"expire_time":1664532960,"last_product_id":"com.ihuman.book.sub.vip1m.trial1w","pack_list":[]};
+
+let key2 = {"allow_guest":1};
+
 
 let obj = JSON.parse($response.body);
 
@@ -42,5 +45,10 @@ if ($request.url.indexOf(path2) != -1){
 if ($request.url.indexOf(path3) != -1){
 	obj.result["vip_status"] = key;
 }
+
+if ($request.url.indexOf(path4) != -1){
+	obj.result["vip_status"] = key2;
+}
+
 
 $done({body: JSON.stringify(obj)});
