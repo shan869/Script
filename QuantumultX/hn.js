1dc907991b84f88749271496cbc37eb38a63bfd1
shan869 <freezhan@gmail.com>
2022-09-29 14:34:53 +0800

    Update hn.js

--- QuantumultX/hn.js
+++ QuantumultX/hn.js
@@ -7,7 +7,7 @@ app 下载地址:https://t.cn/AiCEguqn
 电报群组：https://t.me/f4thorHCC
 QQ资源群：189519867
 QQ吹牛群：316864309
-555
+1010
 
 let key2 = {"allow_guest":1};
 QX:
@@ -67,10 +67,8 @@ if ($request.url.indexOf("/api/v1/getRadical") != -1){
 }
 
 if ($request.url.indexOf("/v3/get_purchase_list") != -1) {
-	 var vi = obj.result.course;
-	var via = obj.result.formula;
-	vi.purchase_time = 1695951991;
-        via.purchase_time = 1695951991;  
+       var vv = obj.result.course{"purchase_time":1695951991};
+       var vb = obj.result.formula{"purchase_time":1695951991}
 	var vip = obj.result.vip;
 	vip.vip_type = 1;
 	vip.expire_time = 1695951991;
