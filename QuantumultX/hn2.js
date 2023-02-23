fd12669f53a2064eefefa29654e51d9fe8d8299c
shan869 <freezhan@gmail.com>
2023-02-23 20:23:57 +0800

    Update hn2.js

--- QuantumultX/hn2.js
+++ QuantumultX/hn2.js
@@ -1,11 +1,15 @@
-const path5 = "/v1/home/books";
+const path5 = "/v1/home?appversion";
 
 let obj = JSON.parse($response.body);
 
-if ($request.url.indexOf(path5) !== -1) {
-  obj.result.bookInfos.forEach((bookInfos) => {
-    bookInfos.book_id = "0011";
-  });
+if ($request.url.indexOf(path5) != -1){
+  
+let freeIds = obj.result.bookInfos.free_id ? JSON.parse(obj.result.bookInfos.free_id) : [];
+for (let i = 5; i <= 30; i++) {
+  freeIds.push(`000${i}`);
+}
+obj.result.bookInfos.free_id = JSON.stringify(freeIds);
+
 }
 
 $done({ body: JSON.stringify(obj) });
