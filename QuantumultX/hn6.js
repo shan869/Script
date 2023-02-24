9f40ff37be41c313a96a421270c8724de33a0ed3
shan869 <freezhan@gmail.com>
2023-02-24 14:36:31 +0800

    Create hn6.js

---
+++ QuantumultX/hn6.js
@@ -0,0 +1,12 @@
+const path1 = "/v3/get_msg";
+const path2 = "/v1/get_user_info";
+
+
+let obj = JSON.parse($response.body);
+
+if ($request.url.indexOf(path1) != -1){
+	obj.result.need_read_flag = true
+}
+	
+	
+$done({body: JSON.stringify(obj)});
