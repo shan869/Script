c10b1626e2144ee581e69e81d730c232b41531e5
shan869 <freezhan@gmail.com>
2023-02-24 15:34:16 +0800

    Update hn6.js

--- QuantumultX/hn6.js
+++ QuantumultX/hn6.js
@@ -5,7 +5,8 @@ const path2 = "/v1/get_user_info";
 let obj = JSON.parse($response.body);
 
 if ($request.url.indexOf(path1) != -1){
-	obj.result.need_read_flag = true
+	obj.result.need_read_flag = true;
+	obj.result.has_more = 1
 }
 	
 	
