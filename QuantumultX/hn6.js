45dd8bbbe6c57efe3a8e33eba43cfe02de02ee1d
shan869 <freezhan@gmail.com>
2023-02-25 08:34:09 +0800

    Update hn6.js

--- QuantumultX/hn6.js
+++ QuantumultX/hn6.js
@@ -1,15 +1,14 @@
-const paths = ["/v3/get_msg"];
-
-if (paths.includes($request.url)) {
-  const { body } = $response;
-  const modifiedBody = JSON.stringify({
-    code: 0,
-    message: "SUCCESS",
-    result: {
-      data: [],
-      has_more: 1,
-      need_read_flag: true
-    }
-  });
-  $done({ body: modifiedBody });
-}
+const path1 = "/v3/get_msg";
+if ($request.url.indexOf(path1) != -1){
+var body = JSON.parse($response.body);
+var obj = {
+  "code": 0,
+  "message": "SUCCESS",
+  "result": {
+    "data": [],
+    "has_more": 0,
+    "need_read_flag": false
+  }
+};
+$done({ body: JSON.stringify(obj) });
+  }
