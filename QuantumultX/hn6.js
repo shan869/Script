8a77a58d36c003348f61ff58b9a6aee4e1da07b5
shan869 <freezhan@gmail.com>
2023-02-25 11:56:33 +0800

    Update hn6.js

--- QuantumultX/hn6.js
+++ QuantumultX/hn6.js
@@ -4,12 +4,23 @@ const path3 = "/v1/get_vip_info";
 const path4 = "/v3/get_home_info";
 const path5 = "/v1/home?appversion";
 
-
-let obj = JSON.parse($response.body);
-
+var body = JSON.parse($response.body);
 
 if ($request.url.indexOf(path1) != -1){
-   obj.result.show_off = false
+   var obj = {
+     "code": 0,
+     "message": "success",
+     "result": [{
+       "name": "inter_app",
+       "show_off": false,
+       "url": "https://static.ihumand.com/h5/shizi/multidisciplinary/index.html#/",
+       "event": "TwoSubjectsNinetyPercent"
+     }, {
+       "name": "pinyin",
+       "show_off": false,
+       "url": "https://m.ihuman.com/h5/words/material-market/index/productDetail/?sceneCode=SC_shizi_sell_pinyin&appchannel=shizishangcheng&source=airplane&airname=pinyin",
+       "event": "SC_shizi_sell_pinyin"
+     }]
+   };
+   $done({body: JSON.stringify(obj)});
 }
-	
-$done({body: JSON.stringify(obj)});
