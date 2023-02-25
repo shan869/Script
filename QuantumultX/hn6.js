8903dc5afa06427a2ebd4128265781b19d7e7ced
shan869 <freezhan@gmail.com>
2023-02-25 22:17:28 +0800

    Update hn6.js

--- QuantumultX/hn6.js
+++ QuantumultX/hn6.js
@@ -3,7 +3,7 @@ const path1 = "/appconfig/bridge";
 let body = JSON.parse($response.body);
 
 if ($request.url.indexOf(path1) !== -1 && body.result) {
-  body.result.type = "1";
+  body.result.miniprogram.type = "1";
 }
 
 $done({body: JSON.stringify(body)});
