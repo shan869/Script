8cfd2601a280c804b92b063f92343e46d9bd84d5
shan869 <freezhan@gmail.com>
2023-02-26 08:40:41 +0800

    Update hn6.js

--- QuantumultX/hn6.js
+++ QuantumultX/hn6.js
@@ -1,9 +1,23 @@
-const path1 = "/appconfig/bridge";
+const path1 = "/log/logReport";
+var body = JSON.parse($response.body);
 
-let body = JSON.parse($response.body);
-
-if ($request.url.indexOf(path1) !== -1 && body.result) {
-  body.result.miniprogram.type = "1";
+if ($request.url.indexOf(path1) !== -1) {
+    var obj = {
+        "resultCode": "103000", 
+        "config": {
+            "sizelimit": "1", 
+            "limitM": "25", 
+            "norlog": "1", 
+            "limitN": "3", 
+            "crashlog": "1", 
+            "limitX": "25", 
+            "fusingTime": "1", 
+            "failLimit": "3", 
+            "timelimit": "2"
+        }, 
+        "desc": "success"
+    };
+    $done({ body: JSON.stringify(obj) });
+} else {
+    $done({ body: JSON.stringify(body) });
 }
-
-$done({body: JSON.stringify(body)});
